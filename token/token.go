0d4cfcd970c5a5b514e39f9b738f117cf4f2c36d
cybugg <timehinsamuel11@gmail.com>
2026-05-23 15:00:52 +0100

    feat(token): add token struct

--- token/token.go
+++ token/token.go
@@ -6,3 +6,29 @@ type token struct {
 	type tokenType
 	Value string
 }
+
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" 
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
