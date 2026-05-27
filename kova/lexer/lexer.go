ab5fb6613a8863dda1e7530952361e6deb102089
cybugg <timehinsamuel11@gmail.com>
2026-05-27 15:40:49 +0100

    fix(lexer): fix lexer switch

--- kova/lexer/lexer.go
+++ kova/lexer/lexer.go
@@ -48,17 +48,16 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
-	default:
-		if isLetter(l.ch){
-			tok.Literal = l.readIdentifier()
-			return tok
-		}
-		else {
-			tok = newToken(token.ILLEGAL, l.ch)
-		}
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
@@ -71,15 +70,14 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 
 // identifier handler
 func (l *Lexer) readIdentifier() string {
-	position := l.position;
-	
-	for isLetter(l.ch){
-		l.readChar();
+	position := l.position
+
+	for isLetter(l.ch) {
+		l.readChar()
 	}
 	return l.input[position:l.position]
 }
 
-
-func isLetter(ch byte) bool{
-	return  'a' <= ch <= "z" ||'A' <= ch <= "Z" || ch == '_'
+func isLetter(ch byte) bool {
+	return 'a' <= ch <= "z" || 'A' <= ch <= "Z" || ch == '_'
 }
