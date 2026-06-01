22e32a2dd4d84eb7b04307f121e8f9d7a4c1cca6
cybugg <timehinsamuel11@gmail.com>
2026-06-01 15:38:32 +0100

    refactor(test): add comments

--- kova/lexer/lexer_test.go
+++ kova/lexer/lexer_test.go
@@ -56,12 +56,18 @@ expectedLiteral string
 }
 l := New(input)
 
+// loop to get all tokens
 for i, tt := range tests {
+
+// 	token
 tok := l.NextToken()
+
+// if the token type is not what we expect then throw
 if tok.Type != tt.expectedType {
 t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 i, tt.expectedType, tok.Type)
 }
+// if the token literal is not what we expect then throw
 if tok.Literal != tt.expectedLiteral {
 t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 i, tt.expectedLiteral, tok.Literal)
