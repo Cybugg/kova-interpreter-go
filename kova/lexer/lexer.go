05830bc6ac30f489f73a01254b11ad4063ddfca6
cybugg <timehinsamuel11@gmail.com>
2026-06-02 19:34:52 +0100

    feat(lexer): update switch to accomadate the new tokens

--- kova/lexer/lexer.go
+++ kova/lexer/lexer.go
@@ -35,20 +35,24 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -98,7 +102,7 @@ func (l *Lexer) skipWhitespace() {
 func (l *Lexer) readNumber() string {
 	position := l.position
 
-	for isDigit(l.ch){
+	for isDigit(l.ch) {
 		l.readChar()
 	}
 	return l.input[position:l.position]
@@ -106,6 +110,6 @@ func (l *Lexer) readNumber() string {
 
 // is number?
 
-func isDigit(ch byte) bool{
+func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
