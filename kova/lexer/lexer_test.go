6be03a23fceca4bf77b050fcd39a055e8311553b
cybugg <timehinsamuel11@gmail.com>
2026-06-04 16:42:33 +0100

    feat(tests): update the tests to fit input

--- kova/lexer/lexer_test.go
+++ kova/lexer/lexer_test.go
@@ -1,6 +1,7 @@
 package lexer
 import (
 "testing"
+
 "github.com/cybug/kova/token"
 )
 
@@ -15,46 +16,63 @@ let result = add(five, ten);
 5 < 10 > 5;
 `
 tests := []struct {
-expectedType token.TokenType
-expectedLiteral string
+	expectedType    token.TokenType
+	expectedLiteral string
 }{
-{token.LET, "let"},
-{token.IDENT, "five"},
-{token.ASSIGN, "="},
-{token.INT, "5"},
-{token.SEMICOLON, ";"},
-{token.LET, "let"},
-{token.IDENT, "ten"},
-{token.ASSIGN, "="},
-{token.INT, "10"},
-{token.SEMICOLON, ";"},
-{token.LET, "let"},
-{token.IDENT, "add"},
-{token.ASSIGN, "="},
-{token.FUNCTION, "fn"},
-{token.LPAREN, "("},
-{token.IDENT, "x"},
-{token.COMMA, ","},
-{token.IDENT, "y"},
-{token.RPAREN, ")"},
-{token.LBRACE, "{"},
-{token.IDENT, "x"},
-{token.PLUS, "+"},
-{token.IDENT, "y"},
-{token.SEMICOLON, ";"},
-{token.RBRACE, "}"},
-{token.SEMICOLON, ";"},
-{token.LET, "let"},
-{token.IDENT, "result"},
-{token.ASSIGN, "="},
-{token.IDENT, "add"},
-{token.LPAREN, "("},
-{token.IDENT, "five"},
-{token.COMMA, ","},
-{token.IDENT, "ten"},
-{token.RPAREN, ")"},
-{token.SEMICOLON, ";"},
-{token.EOF, ""},
+	{token.LET, "let"},
+	{token.IDENT, "five"},
+	{token.ASSIGN, "="},
+	{token.INT, "5"},
+	{token.SEMICOLON, ";"},
+
+	{token.LET, "let"},
+	{token.IDENT, "ten"},
+	{token.ASSIGN, "="},
+	{token.INT, "10"},
+	{token.SEMICOLON, ";"},
+
+	{token.LET, "let"},
+	{token.IDENT, "add"},
+	{token.ASSIGN, "="},
+	{token.FUNCTION, "fn"},
+	{token.LPAREN, "("},
+	{token.IDENT, "x"},
+	{token.COMMA, ","},
+	{token.IDENT, "y"},
+	{token.RPAREN, ")"},
+	{token.LBRACE, "{"},
+	{token.IDENT, "x"},
+	{token.PLUS, "+"},
+	{token.IDENT, "y"},
+	{token.SEMICOLON, ";"},
+	{token.RBRACE, "}"},
+	{token.SEMICOLON, ";"},
+
+	{token.LET, "let"},
+	{token.IDENT, "result"},
+	{token.ASSIGN, "="},
+	{token.IDENT, "add"},
+	{token.LPAREN, "("},
+	{token.IDENT, "five"},
+	{token.COMMA, ","},
+	{token.IDENT, "ten"},
+	{token.RPAREN, ")"},
+	{token.SEMICOLON, ";"},
+
+	{token.BANG, "!"},
+	{token.MINUS, "-"},
+	{token.SLASH, "/"},
+	{token.ASTERISK, "*"},
+	{token.INT, "5"},
+
+	{token.INT, "5"},
+	{token.LT, "<"},
+	{token.INT, "10"},
+	{token.GT, ">"},
+	{token.INT, "5"},
+	{token.SEMICOLON, ";"},
+
+	{token.EOF, ""},
 }
 l := New(input)
 
