a3263c2b710dc482781f638caabc0f8d8dee9a56
cybugg <timehinsamuel11@gmail.com>
2026-06-04 16:52:53 +0100

    feat(lexer): add more token checks to switch

--- kova/lexer/lexer.go
+++ kova/lexer/lexer.go
@@ -53,6 +53,17 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.SEMICOLON, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
