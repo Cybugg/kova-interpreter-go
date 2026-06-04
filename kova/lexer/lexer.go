2a010af62d24d1bcf289edd902a5f7e940075b25
cybugg <timehinsamuel11@gmail.com>
2026-06-04 16:51:06 +0100

    feat(lexer): define token type number check

--- kova/lexer/lexer.go
+++ kova/lexer/lexer.go
@@ -62,7 +62,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
 		} else if isDigit(l.ch) {
+			tok.Type = token.INT
 			tok.Literal = l.readNumber()
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
