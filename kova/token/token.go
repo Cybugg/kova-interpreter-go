819b60386947938a9a642f5ff23c18e54c0660c6
cybugg <timehinsamuel11@gmail.com>
2026-06-05 22:27:10 +0100

    feat(tokens): update keywords map

--- kova/token/token.go
+++ kova/token/token.go
@@ -43,13 +43,18 @@ const (
 	GT       = ">"
 )
 
-var Keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+var keywords = map[string]TokenType{
+"fn":FUNCTION,
+"let":LET,
+"true":TRUE,
+"false":FALSE,
+"if":IF,
+"else":ELSE,
+"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
-	if tok, ok := Keywords[ident]; ok {
+	if tok, ok := keywords[ident]; ok {
 		return tok
 	}
 	return IDENT
